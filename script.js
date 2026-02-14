c721bf1e2edaef4f61874164a9f80dc2b2662cd8
Yuva Naga Saicharan Potharaju <z5687086@ad.unsw.edu.au>
2026-02-14 22:48:40 +1100

    Git finally working

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const sun = document.getElementById("index-sun");
+
+    sun.addEventListener("click", function () {
+        window.location.href = "main.html";
+    });
+
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const logo = document.getElementById("banner-logo");
+
+    logo.addEventListener("click", function () {
+        window.location.href = "index.html";
+    });
+
+});
+
